9ec609950e9e996f8b71c0c48e1fa41da3972248
Brandon Palm <bpalm@redhat.com>
2026-04-08 10:04:02 -0500

    chore: upgrade Go to 1.26.2, add related repos comment

--- main.go
+++ main.go
@@ -1,3 +1,20 @@
+// Package main is the entry point for ztp-dashboard.
+//
+// Related public projects in the sebrandon1 organization:
+//   - https://github.com/sebrandon1/bps-operator
+//   - https://github.com/sebrandon1/compliance-operator-dashboard
+//   - https://github.com/sebrandon1/go-dci
+//   - https://github.com/sebrandon1/go-enphase
+//   - https://github.com/sebrandon1/go-quay
+//   - https://github.com/sebrandon1/go-skylight
+//   - https://github.com/sebrandon1/grab
+//   - https://github.com/sebrandon1/imagecertinfo-operator
+//   - https://github.com/sebrandon1/jiracrawler
+//   - https://github.com/sebrandon1/mirrorbot
+//   - https://github.com/sebrandon1/skylight-bridge
+//   - https://github.com/sebrandon1/testapp
+//   - https://github.com/sebrandon1/tls-compliance-operator
+//   - https://github.com/sebrandon1/yaml-to-readme
 package main
 
 import (
